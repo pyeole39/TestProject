f5a951f5dce6813cbd5a976758e6752f9a79d30a
sai <sai@PiyushYeole>
2023-12-07 13:17:31 +0530

    First Commit

---
+++ jdbc/src/com/progrank/main/main.java
@@ -0,0 +1,11 @@
+package com.progrank.main;
+
+import org.hibernate.cfg.Configuration;
+
+public class main {
+
+	
+	Configuration con = new Configuration();
+	con.
+	
+}
---
+++ jdbc/src/com/progrank/pojo/employee.java
@@ -0,0 +1,28 @@
+package com.progrank.pojo;
+
+public class employee {
+ 
+	
+  private int sid;
+  
+  private String sname,lastname;
+
+public String getSname() {
+	return sname;
+}
+
+public void setSname(String sname) {
+	this.sname = sname;
+}
+
+public String getLastname() {
+	return lastname;
+}
+
+public void setLastname(String lastname) {
+	this.lastname = lastname;
+}
+  
+  
+  
+}
